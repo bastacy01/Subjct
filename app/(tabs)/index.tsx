ffd5707428e662677acdb55614bd364edf7b6ff0
Ben Stacy <ben.stacy98@outlook.com>
2025-06-20 13:38:47 -0400

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -167,7 +167,7 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor: Colors.light.primary[600],
     paddingHorizontal: 24,
-    paddingTop: Platform.OS === 'ios' ? 60 : 40,
+    paddingTop: Platform.OS === 'ios' ? 50 : 30,
     paddingBottom: 24,
     flexDirection: 'row',
     alignItems: 'center',
