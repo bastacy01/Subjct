c87bd232d6d2aea2a1f296614ac874b0d786095f
Ben Stacy <ben.stacy98@outlook.com>
2025-06-28 03:31:34 -0400

    Updated _layout.tsx

--- app/(tabs)/courses/_layout.tsx
+++ app/(tabs)/courses/_layout.tsx
@@ -29,7 +29,7 @@ export default function CoursesLayout() {
               onPress={() => router.back()}
               style={{
                 paddingLeft: 4, // Align with course content below
-                paddingRight: 20,
+                paddingRight: 25,
                 paddingVertical: 12,
                 marginTop: 4,
               }}
